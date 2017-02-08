1e84ed9973cf55e4c95ce6a0cd3b92a0f362f2bc
John Hudson <i@jhudson.io>
2017-02-08 01:22:18 -0600

    Added Sample Entropy.

--- src/algotrading.py
+++ src/algotrading.py
@@ -1,5 +1,6 @@
-import csv, time
-import sample_entropy
+import time
+import operator
+from sample_entropy import sample_entropy
 import numpy as np, argparse
 from scipy.cluster.vq import vq, kmeans, whiten
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser(description='Run algo-trading sim')
 parser.add_argument('file', metavar='F', type=str, help='BTC exchange price data. Containing prices in second column')
 parser.add_argument('--delimiter', metavar='-D', default=',', type=str, help='Delimiter char to use when parsing price data file')
+
 args = parser.parse_args()
 
 print "Parsing price data from file: " + args.file
@@ -18,6 +20,9 @@ print str(len(prices)) + " price points read from data file "
 
 p1 = np.array(prices[0:len(prices)/3])
 p2 = np.array(prices[len(p1):2*len(p1)])
+
+#TODO: parameterize the rest of the algorithm, so that these lengths are not hardcoded into bunch of logic
+timeseries_lengths = [180, 360, 720]
 
 end = len(p1)-721
 S = [np.zeros((end, 181)), np.zeros((end, 361)), np.zeros((end, 721))]
@@ -34,10 +39,10 @@ print 'Clustering..'
 clustering_start = time.time()
 print 'Started: ' + str(clustering_start)
 
+clusters = 100
 
-def clusterSeries(shortTS, mediumTS, longTS):
+def clusterSeries(shortTS, mediumTS, longTS, clusters):
     # kmeans cluster the price vectors in price segment 1
-    clusters = 100
     print 'shape(short TS data): ' + str(np.shape(shortTS))
     print 'shape(medium TS data): ' + str(np.shape(mediumTS))
     print 'shape(long TS data): ' + str(np.shape(longTS))
@@ -49,11 +54,45 @@ def clusterSeries(shortTS, mediumTS, longTS):
     print 'shape(long TS data clusters): ' + str(np.shape(kmean720))
     return kmean180, kmean360, kmean720
 
-
-clusterSeries(S[0][:, 0:180], S[1][:, 0:360], S[2][:, 0:720])
+timeseries_clustered = clusterSeries(S[0], S[1], S[2], clusters)
 
 clustering_end = time.time()
 print 'End    : ' + str(clustering_end)
 print 'Elapsed: ' + str(clustering_end - clustering_start)
 print 'Done clustering..'
 
+#for i in range(0, len(timeseries_clustered)):
+#    assert len(timeseries_clustered[i]) == clusters, \
+#            'time-series cluster array length not equal to expected number of clusters. Expected: %r -- Actual: %r' \
+#            % (clusters, len(timeseries_clustered[i]))
+
+
+print 'Sample Entropy'
+sample_entropy_start = time.time()
+print 'Started : ' + str(sample_entropy_start)
+entropys = []
+for i in range(0, len(timeseries_clustered)):
+    entropys.append({})
+    for k in range(0, len(timeseries_clustered[i])):
+        c_entropy = sample_entropy(timeseries_clustered[i][k], 2)
+        #take sample entropy of m = 2
+        entropys[i].update({c_entropy[1]: timeseries_clustered[i][k]})
+
+    #sort map (key = entropy, value = timeseries) by key and take top 20 values
+    top_20_entropy_ts = sorted(entropys[i].items(), key=operator.itemgetter(0), reverse=True)[0:20]
+    entropys[i] = [x[1] for x in top_20_entropy_ts]
+
+#entropys should now contain array (length 3) of array (length 20) highest entropy time-series (each of length of that particular
+entropys = np.array(entropys)
+sample_entropy_end = time.time()
+print 'End     : ' + str(sample_entropy_end)
+print 'Elapsed : ' + str(sample_entropy_end - sample_entropy_start)
+print 'Done with Sample Entropy...'
+
+#TS (180, 360, 720)
+
+#TODO: bayesian inference
+#TODO: differential evolution optimization
+#TODO: trade testing
+
+#TODO: graph some things???
